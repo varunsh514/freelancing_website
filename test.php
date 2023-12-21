18433c92cb69fc1ec46a5f8278b309ab593f8bac
Varun Sharma <154495611+varunsh514@users.noreply.github.com>
2023-12-21 12:26:43 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,13 @@
+<div class='row shadow-lg rounded-pill ps-3'>
+                    <div class='col-2 p-3'>
+                        <img src='freelance_uploads/<?php echo ''.$record_profile['user_profile_photo'].'';?>' class='img-fluid' style='height:50px;width:50px; border-radius:50%'>
+                    </div>
+                    <div class='col pt-3'>
+                        <span>ldjsfnkjfjsd</span><br>
+                        <span class='text-secondary'>kfjdsfkjdsb</span>
+
+                    </div>
+                    <div class='col pt-4 fw-bold text-end fs-5 pe-5'>
+                        <span>$ 200</span>
+                    </div>
+                </div>
